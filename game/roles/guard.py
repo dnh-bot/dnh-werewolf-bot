9116d27137464d19c78b3a2ce6777a0cdee06af0
nitro2 <ngothanhnhannt@gmail.com>
2021-09-03 20:07:11 +0700

    Refactor to new main code

--- game/roles/guard.py
+++ game/roles/guard.py
@@ -3,10 +3,10 @@ from game.roles.villager import Villager
 import game
 
 
-class Doctor(Villager):
-    # Doctor is basic Villager with ability to protect one person each night
+class Guard(Villager):
+    # Guard is basic Villager with ability to protect one person each night
 
-    def on_role(self):
+    def on_night(self):
         # poll_id = client.show_poll(client.get_personal_channel(self.player), game.get_alive_players())
         # await timeout() or get_poll_result(poll_id)
         pass
--- game/roles/villager.py
+++ game/roles/villager.py
@@ -2,15 +2,12 @@ from game.roles.character import Character, CharacterStatus
 
 
 class Villager(Character):
-    def on_role(self):
-        # poll_id = client.show_poll(client.get_personal_channel(self.player), game.get_alive_players())
-        # await timeout() or get_poll_result(poll_id)
-        pass
 
     def on_day(self):
         # if self.status == CharacterStatus.ALIVE:
         #     client.unmute(self.player)  # unmute Village on day for discussion
         pass
+        # poll_result = self.vote() ???
 
     def on_night(self):
         # client.mute(self.player)  # mute Village on night
