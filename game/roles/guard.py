0fe18abf80cb583b688c2b03825d575bb59702ef
nitro2 <ngothanhnhannt@gmail.com>
2021-09-09 22:23:21 +0700

    Add guard skill

--- game/roles/guard.py
+++ game/roles/guard.py
@@ -8,3 +8,7 @@ class Guard(Villager):
         # poll_id = client.show_poll(client.get_personal_channel(self.player_id), game.get_alive_players())
         # await timeout() or get_poll_result(poll_id)
         pass
+
+
+    async def on_action(self, embed_data):
+        await self.interface.send_embed_to_channel(embed_data, self.channel_name)
