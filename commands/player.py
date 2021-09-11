006e5b7192eaa18d2d80a785738eae5c5b23094f
nitro2 <ngothanhnhannt@gmail.com>
2021-09-11 13:40:38 +0700

    ROnly count alive players in next and end

--- commands/player.py
+++ commands/player.py
@@ -9,7 +9,7 @@ from game import text_template as tt
 
 
 def check_vote_valid(num_votes, num_players, task_name):
-    if num_players < 4:
+    if task_name == "start" and num_players < 4:
         return False, f"At least 4 players to {task_name} game."
 
     if num_votes / num_players <= config.VOTE_RATE:
@@ -63,7 +63,7 @@ async def do_next(game, message, force=False):
                     await message.reply("You are not in the game.")
                 else:
                     game.vote_next.add(message.author.id)
-                    valid, text = check_vote_valid(len(game.vote_next), len(game.players), "next")
+                    valid, text = check_vote_valid(len(game.vote_next), len(game.get_alive_players()), "next")
                     if valid:
                         await game.next_phase()
                     else:
