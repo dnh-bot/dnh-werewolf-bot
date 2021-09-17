cb51711448ce4afbd08208dfc2ee8cc20a80d636
nitro2 <ngothanhnhannt@gmail.com>
2021-09-17 21:39:31 +0700

    Add guide for guard and seer

--- game/roles/seer.py
+++ game/roles/seer.py
@@ -1,5 +1,5 @@
 from game.roles.villager import Villager
-
+from game.text_template import *
 
 class Seer(Villager):
     # Seer can check 1 person each night to see if they are villager/werewolf
@@ -9,4 +9,5 @@ class Seer(Villager):
         self.mana = 1
 
     async def on_action(self, embed_data):
+        await self.interface.send_text_to_channel(generate_before_voting_seer(), self.channel_name)
         await self.interface.send_embed_to_channel(embed_data, self.channel_name)
