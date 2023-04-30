a7ff073368d90e918a55e406ef544660f6ba54ec
neihousaigaai <neihousaigaai@gmail.com>
2023-04-30 22:45:46 +0700

    init heads or tails event

---
+++ game/modes/__init__.py
@@ -0,0 +1,9 @@
+import random
+from typing import List
+
+from config import BOT_PREFIX, TEXT_LANGUAGE, GAMEPLAY_CHANNEL
+import utils
+
+mode_info_dict = utils.common.read_json_file("json/mode_info.json")
+
+
