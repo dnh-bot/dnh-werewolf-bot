6df409a779b28a80e9e34e2a96e27c4eea433263
HUYPC\Huy Bui <buitienhuy02022000@gmail.com>
2024-03-04 01:28:54 +0700

    fix game modes show incorrectly on starting game

--- game/__init__.py
+++ game/__init__.py
@@ -113,6 +113,8 @@ class Game:
         for k, v in modes.items():
             if v == "True":
                 self.modes[k] = True
+            else:
+                self.modes[k] = False
 
         #Backward compatible
         if "allow_guard_self_protection" not in self.modes and "prevent_guard_self_protection" in self.modes:
@@ -182,7 +184,7 @@ class Game:
 
 
             await self.create_channel()
-            await self.interface.send_text_to_channel(text_template.generate_modes(dict(zip(self.modes, map(lambda x: "True", self.modes.values())))), config.GAMEPLAY_CHANNEL)
+            await self.interface.send_text_to_channel(text_template.generate_modes(dict(zip(self.modes, map(lambda x: str(x), self.modes.values())))), config.GAMEPLAY_CHANNEL)
 
             if not self.modes.get("hidden_role"):
                 await self.interface.send_text_to_channel(self.get_role_list(), config.GAMEPLAY_CHANNEL)
