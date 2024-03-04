90a86a4a13e6c5fe9b1707663ff6311bdc8358c4
HUYPC\Huy Bui <buitienhuy02022000@gmail.com>
2024-03-04 17:01:16 +0700

    remove stanley colon

--- game/__init__.py
+++ game/__init__.py
@@ -111,7 +111,7 @@ class Game:
         modes = utils.common.read_json_file("json/game_config.json")
         # Read json dict into runtime dict modes
         for k, v in modes.items():
-            self.modes[k] = v == "True":
+            self.modes[k] = v == "True"
 
         #Backward compatible
         if "allow_guard_self_protection" not in self.modes and "prevent_guard_self_protection" in self.modes:
