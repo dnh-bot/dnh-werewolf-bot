935dbeeec99c69516661074500627804a84acd91
HUYPC\Huy Bui <buitienhuy02022000@gmail.com>
2024-03-07 16:51:04 +0700

    remove cupid_dict param

--- game/__init__.py
+++ game/__init__.py
@@ -470,7 +470,7 @@ class Game:
 
         reveal_list = [(_id, player.__class__.__name__) for _id, player in self.players.items()]
         await self.interface.send_text_to_channel(
-            "\n".join(text_template.generate_reveal_str_list(reveal_list, game_winner, cupid_dict=self.cupid_dict)), config.GAMEPLAY_CHANNEL
+            "\n".join(text_template.generate_reveal_str_list(reveal_list, game_winner, self.cupid_dict)), config.GAMEPLAY_CHANNEL
         )
 
         # write to leaderboard
@@ -482,7 +482,7 @@ class Game:
                     # \u00A0\u00A0 is one space character for discord embed
                     # Put \u200B\n at first of the next field to break line 
                     [f"🎉\u00A0\u00A0\u00A0\u00A0{game_winner}\u00A0\u00A0\u00A0\u00A0🎉"],
-                    text_template.generate_reveal_str_list(reveal_list, game_winner, cupid_dict=self.cupid_dict),
+                    text_template.generate_reveal_str_list(reveal_list, game_winner, self.cupid_dict),
                     [" x ".join(f"<@{player_id}>" for player_id in self.cupid_dict.keys())] if self.cupid_dict else []
                 ],
                 start_time_str=self.start_time.strftime(text_templates.get_format_string("datetime"))
