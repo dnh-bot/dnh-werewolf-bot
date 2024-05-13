f4b207b8559679be710d93e3dc34dc8b2077890e
neihousaigaai <neihousaigaai@gmail.com>
2024-05-14 00:46:29 +0700

    update repr of Reason

--- game/const.py
+++ game/const.py
@@ -18,7 +18,7 @@ class StatusChangeReason(int, Enum):
         return False
 
     def __repr__(self):
-        return self.name.lower()
+        return f"{self.__class__.__name__}.{self.name}"
 
 
 class DeadReason(StatusChangeReason):
