9b9690b0671d8406da6352b6cb425667fd29a2cc
mayd <dxmai2005@gmail.com>
2024-07-21 23:44:37 +0700

    fix set pathologist's target

--- game/roles/pathologist.py
+++ game/roles/pathologist.py
@@ -6,6 +6,9 @@ class Pathologist(Villager):
 
     async def on_night(self):
         pass
+    
+    async def on_day(self):
+        self.target = None
 
     async def on_night_start(self, _, dead_embed_data):
         if self.is_alive():
